9118701d9e5932f37366db9a30a724a03e56024d
dfireBird <aadhishrm11@gmail.com>
2020-09-26 00:24:09 +0530

    implement `SET`, `ALL`, `CLEAR` verbs bonus goals of chapter 6

--- lib/tork.ex
+++ lib/tork.ex
@@ -24,17 +24,20 @@ defmodule Tork do
   end
 
   defp parse(conn, command) do
-    map = %{hello: "there",
-            general: "kenobi",
-            foobal: "soccer",
-            elixir: "good",
-           }
-    case String.split(command) do
+    command = String.trim(command)
+    case String.split(command, " ", parts: 3) do
       ["GET", key] ->
-        case map[String.to_atom(key)] do
+        case Tork.Map.get(key) do
           nil -> :gen_tcp.send(conn, "ERROR undefined\n")
           value -> :gen_tcp.send(conn, "ANSWER #{value}\n")
         end
+
+      ["SET", key, value] ->
+        Tork.Map.set(key, value); :gen_tcp.send(conn, "OK\n")
+      ["CLEAR"] -> Tork.Map.clear(); :gen_tcp.send(conn, "OK\n")
+      ["ALL"] ->
+        :gen_tcp.send(conn, "#{Enum.join(Tork.Map.all(), "\n")}"); :gen_tcp.send(conn, "\nOK\n")
+      _ -> :gen_tcp.send(conn, "UNKNOWN COMMAND\n")
     end
   end
 end
