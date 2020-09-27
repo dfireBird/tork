040482f9f21d98672cc0053ed8a9d9f4b0e5d995
dfireBird <aadhishrm11@gmail.com>
2020-09-27 22:33:18 +0530

    parse http method request and rename modules goals and bonus goals of chapter 10

--- lib/tork.ex
+++ lib/tork.ex
@@ -1,4 +1,5 @@
 defmodule Tork do
+  require Logger
   @moduledoc """
   The main module for the application which starts the server,
   accept clients and send and receive message.
@@ -9,6 +10,7 @@ defmodule Tork do
   """
   def start(port) do
     {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info("Accepting connections on the port #{port}")
 
     accept(socket)
   end
@@ -16,7 +18,6 @@ defmodule Tork do
   defp accept(socket) do
     {:ok, conn} = :gen_tcp.accept(socket)
 
-    :gen_tcp.send(conn, "Welcome!\n")
     {:ok, pid} = Task.Supervisor.start_child(Tork.TaskSupervisor, fn -> recv(conn) end)
     :ok = :gen_tcp.controlling_process(conn, pid)
     accept(socket)
@@ -25,23 +26,19 @@ defmodule Tork do
   defp recv(conn) do
     msg =
       with {:ok, data} <- read_line(conn),
-           {:ok, command} <- Tork.Verb.parse(data),
-           do: Tork.Verb.run(command, Tork.Map)
+           {:ok, command} <- Tork.Method.parse(data),
+           do: Tork.Method.run(command)
     write_line(conn, msg)
-    recv(conn)
-  end
+   end
 
   defp read_line(socket) do
     :gen_tcp.recv(socket, 0)
   end
 
-  defp write_line(socket, {:ok, text}) do
-    :gen_tcp.send(socket, text)
-  end
-
   # Known error send it to client
   defp write_line(socket, {:error, :unknown_command}) do
-    :gen_tcp.send(socket, "UNKNOWN COMMAND\n")
+    Logger.error("Unknown Method")
+    :gen_tcp.close(socket)
   end
 
   # The client closed the connection, exit cleanly
@@ -54,4 +51,9 @@ defmodule Tork do
     :gen_tcp.send(socket, "ERROR\n")
     exit(error)
   end
+
+  defp write_line(socket, _) do
+    :gen_tcp.close(socket)
+  end
+
 end
---
+++ lib/tork/method.ex
@@ -0,0 +1,63 @@
+defmodule Tork.Method do
+  require Logger
+  @moduledoc """
+  Modules that parses the data sent by client, return messages related
+  to commands and runs the command.
+  """
+
+
+  @doc ~S"""
+  Parses the given `data` into command.
+  """
+  def parse(data) do
+    Logger.info("Message recieved: #{data}")
+    case String.split(data) do
+      ["GET", resource, version] -> {:ok, {:get, resource, version}}
+      ["PUT", resource, version] -> {:ok, {:put, resource, version}}
+      ["HEAD", resource, version] -> {:ok, {:head, resource, version}}
+      ["POST", resource, version] -> {:ok, {:post, resource, version}}
+      ["DELETE", resource, version] -> {:ok, {:delete, resource, version}}
+      ["OPTIONS", resource, version] -> {:ok, {:options, resource, version}}
+      ["TRACE", resource, version] -> {:ok, {:trace, resource, version}}
+      ["CONNECT", resource, version] -> {:ok, {:connect, resource, version}}
+      _ -> {:error, :unknown_command}
+    end
+  end
+
+  @doc """
+  Runs the given `command`.
+  """
+  def run(command)
+
+  def run({:get, resource, version}) do
+    Logger.info("Got GET request for #{resource} with #{version}")
+  end
+
+  def run({:put, resource, version}) do
+    Logger.info("Got PUT request for #{resource} with #{version}")
+  end
+
+  def run({:head, resource, version}) do
+    Logger.info("Got HEAD request for #{resource} with #{version}")
+  end
+
+  def run({:post, resource, version}) do
+    Logger.info("Got POST request for #{resource} with #{version}")
+  end
+
+  def run({:delete, resource, version}) do
+    Logger.info("Got DELETE request for #{resource} with #{version}")
+  end
+
+  def run({:options, resource, version}) do
+    Logger.info("Got OPTIONS request for #{resource} with #{version}")
+  end
+
+  def run({:trace, resource, version}) do
+    Logger.info("Got TRACE request for #{resource} with #{version}")
+  end
+
+  def run({:connect, resource, version}) do
+    Logger.info("Got CONNECT request for #{resource} with #{version}")
+  end
+end
